e73c17cd991d08d5afd9a353ad0f6d85047d588c
AymanRagab2006 <aymanr2710@gmail.com>
2025-12-24 15:40:42 +0200

    Menu Library

---
+++ menu.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "menu.h"
+
+void displayMenu() {
+    printf("=== Bank Management System ===\n");
+    printf("1. Add New Account\n");
+    printf("2. Delete Account\n");
+    printf("3. Modify Account Data\n");
+    printf("4. Search by Account Number\n");
+    printf("5. Search by Name\n");
+    printf("6. Change Account Status\n");
+    printf("7. Withdraw Amount\n");
+    printf("8. Deposit Amount\n");
+    printf("9. Tranfer Amount\n");
+    printf("10. Transaction Report\n");
+    printf("11. Display All Accounts\n");
+    printf("12. Quit\n");
+    
+    printf("==============================\n");
+}
