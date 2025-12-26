d4bbc11b5addddd8981ec69afb1c1e2c0c06d910
AliMohamed3122005 <alymohamed220055@gmail.com>
2025-12-26 23:30:48 +0200

    Update load_Accounts.h

--- load_Accounts.h
+++ load_Accounts.h
@@ -1,7 +1,7 @@
-#ifndef load_Accounts_H
-#define load_Accounts_H
+#ifndef LOAD_ACCOUNTS_H
+#define LOAD_ACCOUNTS_H
 #include "system_structs.h"
 
-void loadaccounts(Account acc[], int max);
+void loadaccounts(Account acc[], int max, int *count);
 
 #endif
