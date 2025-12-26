8143fdd4e239079f050d31727ad35f0ca32dc57b
AliMohamed3122005 <alymohamed220055@gmail.com>
2025-12-26 23:31:48 +0200

    Update load_Accounts.c

--- load_Accounts.c
+++ load_Accounts.c
@@ -2,33 +2,38 @@
 #include <string.h>
 #include "system_structs.h"
 
-int loadaccounts(Account acc[], int max)
+void loadaccounts(Account acc[], int max, int *count)
 {
-    int fields;
-    FILE *f = fopen("accounts.txt", "r");
-    if (f == NULL)
+    FILE *f = fopen("accountss.txt", "r");
+    if (!f)
     {
-        printf("Error opening file!!!!!\n");
-        return 0;
+        printf("Error: file not found!\n");
+        *count = 0;
+        return;
     }
-    int count = 0;
+    else
+    {
+        printf("File opened successfully.\n");
+    }
+
+    *count = 0;
     char line[400];
-    while (count < max && fgets(line, sizeof(line), f)) /*sscanf evaluate the buffer (line) to strings and numbers */
+    while (*count < max && fgets(line, sizeof(line), f))
     {
-        fields = sscanf(line, "%i,%[^,],%[^,],%s,%f,%i,%i,%i",
-                        &acc[count].accountNumber,
-                        acc[count].name,
-                        acc[count].email,
-                        acc[count].mobileNumber,
-                        &acc[count].balance,
-                        &acc[count].dateOpened.month,
-                        &acc[count].dateOpened.year,
-                        &acc[count].status);
-    }
-    if (fields == 8)
-    {
-        count++;
+        line[strcspn(line, "\r\n")] = 0; // remove newline
+
+        int fields = sscanf(line, "%lld,%49[^,],%49[^,],%10[^,],%f,%d,%d,%9s",
+                            &acc[*count].accountNumber,
+                            acc[*count].name,
+                            acc[*count].email,
+                            acc[*count].mobileNumber,
+                            &acc[*count].balance,
+                            &acc[*count].dateOpened.month,
+                            &acc[*count].dateOpened.year,
+                            acc[*count].status);
+
+        if (fields == 8)
+            (*count)++;
     }
     fclose(f);
-    return count;
 }
