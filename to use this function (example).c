6997430dacccf4ab644cc354a9a063bf3c9138e5
Nour <nooramr20005@gmail.com>
2025-12-27 00:14:03 +0200

    Enhance account search with user confirmation
    Added user prompt to print account details if found.

--- to use this function (example).c
+++ to use this function (example).c
@@ -1,13 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "system_structs.h"
+#include "load_Accounts.h"
+#include "searchByAccountNumber.h"
+
 int main()
 {
-    Account accounts[100]; 
+    Account accounts[100]; // array to hold accounts
     Account *foundAccount;
     int coun = 0;
-    loadaccounts(accounts, 100, &coun); 
+    loadaccounts(accounts, 100, &coun); // Load accounts from file
     searchByAccountNumber(accounts, coun, 1000000001, &foundAccount);
     if (foundAccount != NULL)
     {
-        printf("Account found: %s\n", (*foundAccount).name);
+        char choice;
+        printf("Account found. Print details? (y/n): ");
+        scanf(" %c", &choice);
+
+        if (choice == 'y' || choice == 'Y')
+        {
+            printAccount(foundAccount);
+        }
     }
     else
     {
