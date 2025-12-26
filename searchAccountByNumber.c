8b3271d084c4b131a6e379c50068db37eec01388
Nour <nooramr20005@gmail.com>
2025-12-27 00:01:36 +0200

    Modify searchByAccountNumber to return account pointer
    Updated searchByAccountNumber to return found account.

--- searchAccountByNumber.c
+++ searchAccountByNumber.c
@@ -2,35 +2,16 @@
 #include <string.h>
 #include "system_structs.h"
 
-void searchByAccountNumber(Account acc[], int count, long target)
+void searchByAccountNumber(Account acc[], int count, int target, Account **result)
 {
-    int found = 0;
+    *result = NULL;
 
     for (int i = 0; i < count; i++)
     {
         if (acc[i].accountNumber == target)
         {
-            printf("Account Number : %ld\n", acc[i].accountNumber);
-            printf("Name           : %s\n", acc[i].name);
-            printf("E-mail         : %s\n", acc[i].email);
-            printf("Balance        : %.2f $\n", acc[i].balance);
-            printf("Mobile         : %s\n", acc[i].mobileNumber);
-
-            char *months[] = {
-                "January", "February", "March", "April", "May", "June",
-                "July", "August", "September", "October", "November", "December"};
-
-            printf("Date Opened    : %s %d\n", months[acc[i].dateOpened.month - 1], acc[i].dateOpened.year);
-
-            printf("Status         : %s\n", acc[i].status);
-
-            found = 1;
-            break;
+            *result = &acc[i]; // store address of found account
+            return;
         }
-    }
-
-    if (!found)
-    {
-        printf("Account not found.\n");
     }
 }
