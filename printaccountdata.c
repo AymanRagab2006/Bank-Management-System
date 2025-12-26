ca85f574139fa3b6fdfd8f353b5d5912a19eb30c
Nour <nooramr20005@gmail.com>
2025-12-27 00:13:52 +0200

    Create printaccountdata.c

---
+++ printaccountdata.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "system_structs.h"
+
+void printAccount(const Account *acc)
+{
+    char *months[] = {
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"};
+
+    printf("Account Number : %d\n", acc->accountNumber);
+    printf("Name           : %s\n", acc->name);
+    printf("E-mail         : %s\n", acc->email);
+    printf("Mobile         : %s\n", acc->mobileNumber);
+    printf("Balance        : %.2f $\n", acc->balance);
+    printf("Date Opened    : %s %d\n", months[acc->dateOpened.month - 1], acc->dateOpened.year);
+    printf("Status         : %s\n", acc->status ? "Active" : "Inactive");
+}
