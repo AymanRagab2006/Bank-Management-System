983fd4c005dc8f67ee08179aca5a2184a90161f9
Nour <nooramr20005@gmail.com>
2025-12-27 23:13:07 +0200

    Update searchAccountByName.c

--- searchAccountByName.c
+++ searchAccountByName.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 #include "system_structs.h"
-Account *searchByAccountName(Account acc[], int count,  char target[])
+void searchByAccountName(Account acc[], int count, char target[])
 {
+    int cal = 0;
     for (int i = 0; i < count; i++)
     {
         if (strcmp(acc[i].name, target) == 0)
         {
-            return &acc[i];
+            printAccount(&acc[i]);
+            printf("________________________________\n");
         }
     }
-    return NULL;
+}
+int main()
+{
+    int coun = 0;
+    Account accounts[100];
+    loadaccounts(accounts, 100, &coun);
+    printf("Loaded %d accounts.\n", coun);
+    searchByAccountName(accounts, coun, "ali");
+    return 0;
 }
