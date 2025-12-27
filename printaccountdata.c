e01ffea99830d6efc5dee796c2913d47e8a50f1e
Nour <nooramr20005@gmail.com>
2025-12-27 14:58:23 +0200

    Update printaccountdata.c

--- printaccountdata.c
+++ printaccountdata.c
@@ -7,10 +7,10 @@ void printAccount(const Account *acc)
         "January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"};
 
-    printf("Account Number : %d\n", acc->accountNumber);
+    printf("Account Number : %ld\n", acc->accountNumber);
     printf("Name           : %s\n", acc->name);
     printf("E-mail         : %s\n", acc->email);
-    printf("Mobile         : %s\n", acc->mobileNumber);
+    printf("Mobile         : %ld\n", acc->mobileNumber);
     printf("Balance        : %.2f $\n", acc->balance);
     printf("Date Opened    : %s %d\n", months[acc->dateOpened.month - 1], acc->dateOpened.year);
     printf("Status         : %s\n", acc->status ? "Active" : "Inactive");
