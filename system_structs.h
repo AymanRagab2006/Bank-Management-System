6122ec1e6598a2063642cdae084c3a51772e5ffc
Nour <nooramr20005@gmail.com>
2025-12-27 16:43:44 +0200

    Create system_structs.h

---
+++ system_structs.h
@@ -0,0 +1,22 @@
+#ifndef SYSTEM_STRUCTS_H
+#define SYSTEM_STRUCTS_H
+
+typedef struct
+{
+    int month;
+    int year;
+} Date;
+
+typedef struct
+{
+    long long accountNumber;
+    char name[50];
+    char email[50];
+    long long mobileNumber;
+    float balance;
+    Date dateOpened;
+    char status[10]; // 1 for active, 0 for inactive
+} Account;
+void printAccount(const Account *acc);
+
+#endif
