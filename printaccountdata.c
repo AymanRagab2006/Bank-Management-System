ec7694bead145d3c7f19f0fe4558b4d0d0c9b812
Nour <nooramr20005@gmail.com>
2025-12-28 03:18:00 +0200

    Update printaccountdata.c

--- printaccountdata.c
+++ printaccountdata.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include "system_structs.h"
+#include <string.h>
+#include <stdlib.h>
+#include "load_Accounts.h"
 
 void printAccount(const Account *acc)
 {
@@ -7,10 +10,10 @@ void printAccount(const Account *acc)
         "January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"};
 
-    printf("Account Number : %ld\n", acc->accountNumber);
+    printf("Account Number : %lld\n", acc->accountNumber);
     printf("Name           : %s\n", acc->name);
     printf("E-mail         : %s\n", acc->email);
-    printf("Mobile         : %ld\n", acc->mobileNumber);
+    printf("Mobile         : %s\n", acc->mobileNumber);
     printf("Balance        : %.2f $\n", acc->balance);
     printf("Date Opened    : %s %d\n", months[acc->dateOpened.month - 1], acc->dateOpened.year);
     printf("Status         : %s\n", acc->status ? "Active" : "Inactive");
